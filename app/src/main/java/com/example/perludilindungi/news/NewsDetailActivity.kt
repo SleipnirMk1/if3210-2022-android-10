ade79d3ede8c275247927a73a117a42bc65b331a
Raffi Zulvian Muzhaffar <60030585+raffizulvian@users.noreply.github.com>
2022-03-05 13:54:12 +0700

    feat: add WebView functionality
    finnaly successfully display web page on WebView

--- app/src/main/java/com/example/perludilindungi/news/NewsDetailActivity.kt
+++ app/src/main/java/com/example/perludilindungi/news/NewsDetailActivity.kt
@@ -1,5 +1,36 @@
 package com.example.perludilindungi.news
 
-class NewsDetailActivity {
-    // TODO("Implement detail activity")
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.KeyEvent
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import com.example.perludilindungi.R
+import com.example.perludilindungi.models.NewsResultResponse
+
+class NewsDetailActivity : AppCompatActivity() {
+    private val NEWS_TAG = "NewsDetail"
+    private lateinit var myWebView: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news_detail)
+
+        myWebView = findViewById(R.id.news_web_view)
+        myWebView.webViewClient = WebViewClient()
+
+        val link = intent.getStringExtra("EXTRA_LINK")
+        myWebView.loadUrl(link!!)
+
+        Log.d(NEWS_TAG, "onCreate: called")
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_BACK && myWebView.canGoBack()) {
+            myWebView.goBack()
+            return true
+        }
+        return super.onKeyDown(keyCode, event)
+    }
 }
